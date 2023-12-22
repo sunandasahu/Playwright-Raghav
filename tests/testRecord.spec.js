23a60fe323698ffa9cc2d356de7401f801f7227b
Sunanda Sahu <sunanda.sahu@bettercloud.com>
2023-12-22 16:12:33 -0500

    new branch-01

---
+++ tests/testRecord.spec.js
@@ -0,0 +1,23 @@
+
+import {test, expect} from '@playwright/test';
+const{ chromium } = require('@playwright/test')
+
+test('Recorded test',async () => {
+  const browser = await chromium.launch({
+    headless: false
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.getByRole('link', { name: 'Logout' }).click();
+
+  // ---------------------
+  await context.close();
+  await browser.close();
+});
